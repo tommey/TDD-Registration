537c81ba8c8b350210f5994b581a59f5fc63b9b4
tommey <github@tommey.hu>
2014-07-31 14:16:10 +0200

    Extended Factory with the new classes

--- src/Factory.php
+++ src/Factory.php
@@ -33,9 +33,18 @@ class Factory
 		);
 	}
 
+	public function getUserTypeValidator()
+	{
+		return new UserTypeValidator();
+	}
+
 	public function getUserValidator()
 	{
-		return new UserValidator($this->getEmailValidator(), $this->getPasswordValidator());
+		return new UserValidator(
+			$this->getEmailValidator(),
+			$this->getPasswordValidator(),
+			$this->getUserTypeValidator()
+		);
 	}
 
 	public function getPasswordGenerator()
@@ -44,6 +53,31 @@ class Factory
 			$this->configuration->get(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_MINIMUM_LENGTH),
 			$this->configuration->get(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_MAXIMUM_LENGTH),
 			$this->configuration->get(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_CHARACTER_SET)
+		);
+	}
+
+	public function getPasswordHasher()
+	{
+		return new PasswordHasher();
+	}
+
+	public function getUserPersistentStorage()
+	{
+		return new SqliteStorage($this->configuration->get(Key::CONFIGURATION_STORAGE_USER_DATABASE_CONFIGURATION));
+	}
+
+	public function getUserRepository()
+	{
+		return new UserRepository($this->getUserPersistentStorage());
+	}
+
+	public function getRegistrationModule()
+	{
+		return new RegistrationModule(
+			$this->getUserRepository(),
+			$this->getUserValidator(),
+			$this->getPasswordHasher(),
+			$this->getPasswordGenerator()
 		);
 	}
 }
