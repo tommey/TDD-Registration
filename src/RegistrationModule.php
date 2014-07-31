65a5b68f1fd12735348546a76af6ce21648fe8bd
tommey <github@tommey.hu>
2014-07-31 14:15:53 +0200

    Added RegistrationModule

---
+++ src/RegistrationModule.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tdd;
+
+class RegistrationModule
+{
+	/** @var UserValidator */
+	private $userValidator;
+	/** @var UserRepository */
+	private $userRepository;
+	/** @var PasswordHasher */
+	private $passwordHasher;
+	/** @var PasswordGenerator */
+	private $passwordGenerator;
+
+	public function __construct($userRepository, $userValidator, $passwordHasher, $passwordGenerator)
+	{
+		$this->userRepository    = $userRepository;
+		$this->userValidator     = $userValidator;
+		$this->passwordHasher    = $passwordHasher;
+		$this->passwordGenerator = $passwordGenerator;
+	}
+
+	public function registerLocalUser($email, $password)
+	{
+		return $this->registerUser($email, $password, 'local');
+	}
+
+	public function registerExternalUser($email, $type)
+	{
+		return $this->registerUser($email, $this->passwordGenerator->generate(), $type);
+	}
+
+	private function registerUser($email, $password, $type)
+	{
+		$rawUser = new User($email, $password, $type);
+
+		if (!$this->userValidator->isValid($rawUser))
+		{
+			throw new \InvalidArgumentException('User details are invalid!');
+		}
+
+		$encodedUser = new User($email, $this->passwordHasher->hash($password), $type);
+
+		return $this->userRepository->save($encodedUser);
+	}
+}
