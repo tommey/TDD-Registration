a149da86cd92b2698a454cb3ff17433e70de64d8
tommey <github@tommey.hu>
2014-07-31 14:11:02 +0200

    Added PersistentStorage default fetch option to be FETCH_ASSOC

--- src/PersistentStorage.php
+++ src/PersistentStorage.php
@@ -4,4 +4,10 @@ namespace Tdd;
 
 abstract class PersistentStorage extends \PDO
 {
+	public function __construct($dsn, $username = null, $password = null, array $options = array())
+	{
+		$options[\PDO::ATTR_DEFAULT_FETCH_MODE] = \PDO::FETCH_ASSOC;
+
+		parent::__construct($dsn, $username, $password, $options);
+	}
 }
