3d2b2911c6cb6fc4ff35c318b4ce7ebdd093f25e
tommey <github@tommey.hu>
2014-08-05 12:19:59 +0200

    More fix for code climate test reporter

--- bin/code-climate-test-reporter.sh
+++ bin/code-climate-test-reporter.sh
@@ -5,7 +5,7 @@
 REPORT_FILE="build/logs/codeclimate.json"
 
 # Run report generator.
-bash $(dirname $0)/../vendor/bin/test-reporter --stdout > ${REPORT_FILE}
+php $(dirname $0)/../vendor/codeclimate/php-test-reporter/composer/bin/test-reporter --stdout > ${REPORT_FILE}
 
 # Send report to code climate.
 curl -X POST -d @${REPORT_FILE} -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.2)' https://codeclimate.com/test_reports
