192b5342ad13b27977d7144eb1012102742b919e
tommey <github@tommey.hu>
2014-08-05 09:56:26 +0200

    Added fake db file handling for the RegistrationTest

--- test/Integration/RegistrationTest.php
+++ test/Integration/RegistrationTest.php
@@ -9,6 +9,8 @@ use Tdd\Factory;
 
 class RegistrationTest extends \PHPUnit_Framework_TestCase
 {
+	/** @var string Temporary database file (copy of the real one) to use for testing. */
+	private $databaseFile;
 	/** @var Configuration */
 	private $configuration;
 	/** @var Factory */
@@ -20,12 +22,35 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase
 	{
 		$_GET = $_POST = array();
 
+		// Copy real, empty database file.
+		$realDatabaseFile = __DIR__ . '/../../resource/user.db';
+		$testDatabaseFile = $realDatabaseFile . '-test';
+
+		if (!file_exists($realDatabaseFile))
+		{
+			throw new \LogicException('Real database file not found for test!');
+		}
+
+		if (file_exists($testDatabaseFile))
+		{
+			throw new \LogicException('Test database file found for test, but it should not be there...');
+		}
+
+		if (!copy($realDatabaseFile, $testDatabaseFile))
+		{
+			throw new \LogicException('Could not copy real database file to test!');
+		}
+
+		$this->databaseFile = $testDatabaseFile;
+
+
+		// Setup application.
 		$this->configuration = new Configuration();
 
 		$this->configuration->set(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_MINIMUM_LENGTH, 6);
 		$this->configuration->set(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_MAXIMUM_LENGTH, 64);
 		$this->configuration->set(Key::CONFIGURATION_VALIDATOR_USER_PASSWORD_CHARACTER_SET, 'qwertzuiopasdfghjklyxcvbnm');
-		$this->configuration->set(Key::CONFIGURATION_STORAGE_USER_DATABASE_CONFIGURATION, array('file' => __DIR__ . '/../../resource/user.db'));
+		$this->configuration->set(Key::CONFIGURATION_STORAGE_USER_DATABASE_CONFIGURATION, array('file' => $testDatabaseFile));
 
 		$this->factory     = new Factory($this->configuration);
 		$this->application = new Application($this->factory);
@@ -34,6 +59,7 @@ class RegistrationTest extends \PHPUnit_Framework_TestCase
 	public function tearDown()
 	{
 		unset($this->application, $this->configuration);
+		unlink($this->databaseFile);
 	}
 
 	public function testLocalUserCanBeRegistered()
