49d1d41fa7303bc8b1c25f2d341c4d17ece4e0de
tommey <github@tommey.hu>
2014-08-05 12:00:39 +0200

    Added scrutinizer-ci test reporting + Fixed code climate test reporting

--- bin/code-climate-test-reporter.sh
+++ bin/code-climate-test-reporter.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # Code climate report generator and sender - hack over SSL cert problem.
 
+REPORT_FILE="build/logs/codeclimate.json"
+
 # Run report generator.
-./vendor/bin/test-reporter --stdout > build/log/codeclimate.json
+bin/bash $(dirname $0)/../vendor/bin/test-reporter --stdout > ${REPORT_FILE}
 
 # Send report to code climate.
-curl -X POST -d @build/log/codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.2)' https://codeclimate.com/test_reports
+curl -X POST -d @${REPORT_FILE} -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.2)' https://codeclimate.com/test_reports
